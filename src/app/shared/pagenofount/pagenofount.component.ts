4b41a27a1fe9605026c913fd0e064167c9279c60
EDSON\Edson <edson.linares1@gmail.com>
2018-03-01 19:06:03 -0500

    primer commit

---
+++ src/app/shared/pagenofount/pagenofount.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagenofount',
+  templateUrl: './pagenofount.component.html',
+  styles: []
+})
+export class PagenofountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
